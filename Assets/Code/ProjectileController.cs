d6d348dd5dce19e5b12d9558da434c1af75540df
AsherLaakes <asherl@live.ca>
2017-01-20 22:03:49 -0500

    added some ui

---
+++ Assets/Code/ProjectileController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Code
+{
+    class ProjectileController : MonoBehaviour
+    {
+        public virtual void Activate()
+        {
+            
+        }
+    }
+}
