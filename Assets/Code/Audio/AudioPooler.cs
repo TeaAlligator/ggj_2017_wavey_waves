ba464a3d3b72071ae83ab72ff2c74aa6f11e994f
AsherLaakes <asherl@live.ca>
2017-01-22 13:49:39 -0500

    audio pooling code

---
+++ Assets/Code/Audio/AudioPooler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Code.Audio
+{
+    public class PooledAudioRequest
+    {
+        public AudioClip Sound;
+        public float Volume;
+        public Vector3 Target;
+        public bool IsLoop;
+        public PooledAudioRequest Next;
+
+        public Action OnFinished;
+
+        public PooledAudioRequest()
+        {
+            Volume = 1f;
+            IsLoop = false;
+        }
+    }
+
+    public class AudioToken
+    {
+        public bool IsCurrentlyActive;
+
+        public Action<float> AdjustVolume;
+        public Func<PooledAudioRequest, AudioToken> Replace;
+        public Action End;
+        public Action<float> TrailOff;
+    }
+
+    public class AudioPooler : MonoBehaviour, IResolveable
+    {
+        [SerializeField] private GameObject _audioSourcePrefab;
+        [SerializeField] private Transform _sourceParent;
+        private readonly PooledAudioSource[] _sources;
+
+        private const int NumberOfSources = 32;
+
+        protected void Awake()
+        {
+            for (var i = 0; i < NumberOfSources; i++)
+            {
+                _sources[i] = Instantiate(_audioSourcePrefab).GetComponent<PooledAudioSource>();
+                _sources[i].transform.SetParent(_sourceParent.transform);
+            }
+        }
+
+        public AudioToken PlaySound(PooledAudioRequest request)
+        {
+            for (var i = 0; i < NumberOfSources; i++)
+                if (!_sources[i].IsActive)
+                {
+                    var source = _sources[i];
+                    return source.PlaySound(request);
+                }
+            
+            return new AudioToken
+            {
+                IsCurrentlyActive = false,
+
+                Replace = newRequest => PlaySound(newRequest),
+                End = () => { },
+                TrailOff = time => { },
+                AdjustVolume = volume => { }
+            };
+        }
+
+        public void KillAllSounds()
+        {
+            foreach (var source in _sources)
+                source.Stop();
+        }
+    }
+}
---
+++ Assets/Code/Audio/PooledAudioSource.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class PooledAudioSource : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audio;
+
+        /* PROPERTIES */
+        private PooledAudioRequest _currentRequest;
+        public bool IsActive { get { return _currentRequest != null; } }
+
+        // lerpy loo
+        private bool _isTrailingOff;
+        private float _trailOffTime;
+        private float _trailOffProgress;
+        
+        public AudioToken PlaySound(PooledAudioRequest request)
+        {
+            _isTrailingOff = false;
+            if (_currentRequest != null && _currentRequest.OnFinished != null)
+                _currentRequest.OnFinished();
+            _currentRequest = request;
+
+            // bail gracefully if request is null
+            if (request == null)
+                return new AudioToken
+                {
+                    IsCurrentlyActive = false,
+
+                    Replace = replacementRequest => PlaySound(replacementRequest),
+                    End = () => { },
+                    TrailOff = time => { },
+                    AdjustVolume = volume => { }
+                };
+
+            _audio.loop = request.Next == null && request.IsLoop;
+            _audio.transform.position = request.Target;
+
+            _audio.clip = request.Sound;
+            _audio.volume = request.Volume;
+            _audio.Play();
+
+            var token = new AudioToken
+            {
+                IsCurrentlyActive = true,
+
+                Replace = replacementRequest => PlaySound(replacementRequest),
+                TrailOff = time => TrailOff(time),
+                AdjustVolume = volume =>
+                {
+                    if (_currentRequest != null)
+                    {
+                        _currentRequest.Volume = volume;
+                        _audio.volume = _currentRequest.Volume;
+                    }
+                }
+            };
+            token.End = () =>
+            {
+                Stop();
+                token.IsCurrentlyActive = false;
+            };
+            return token;
+        }
+
+        public void Stop()
+        {
+            _isTrailingOff = false;
+            _audio.Stop();
+            _currentRequest = null;
+        }
+
+        public void TrailOff(float time)
+        {
+            if (_isTrailingOff) return;
+
+            _isTrailingOff = true;
+            _trailOffProgress = 0f;
+            _trailOffTime = time;
+        }
+        
+        protected void Update()
+        {
+            if (_currentRequest == null) return;
+
+            if (_isTrailingOff)
+            {
+                _audio.volume = _currentRequest.Volume * (1 - (_trailOffProgress / _trailOffTime));
+                _trailOffProgress += Time.deltaTime;
+
+                if (_trailOffProgress >= _trailOffTime)
+                {
+                    if (_currentRequest.OnFinished != null)
+                        _currentRequest.OnFinished();
+                    Stop();
+                }
+            }
+
+            // once finished, we move on to the next sound
+            // or it will be null (both are cool)
+            else if (!_audio.isPlaying)
+            {
+                if (_currentRequest.Next != null)
+                    PlaySound(_currentRequest.Next);
+                else if (_currentRequest.OnFinished != null)
+                    _currentRequest.OnFinished();
+                else
+                    Stop();
+            }
+        }
+    }
+}
