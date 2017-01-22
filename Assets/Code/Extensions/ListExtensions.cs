c3d500d991a83af5ff7e7b63ddfd4aa2b8e0e5d2
AsherLaakes <asherl@live.ca>
2017-01-22 14:12:31 -0500

    get random item list extension

---
+++ Assets/Code/Extensions/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Code.Extensions
+{
+    public static class ListExtensions
+    {
+        private readonly static Random Rand = new Random();
+
+        public static T GetRandomItem<T>(this List<T> input)
+        {
+            if (input.Count == 0)
+                Debug.Log("WARNING! GetRandomItem called with an empty list!");
+
+            var index = (int)(Rand.NextDouble() * input.Count);
+            return input[index > input.Count - 1 ? input.Count - 1 : index];
+        }
+    }
+}
