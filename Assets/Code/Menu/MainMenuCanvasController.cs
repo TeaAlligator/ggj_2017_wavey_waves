93443f6da4cdea1693975346c73235b04248082a
AsherLaakes <asherl@live.ca>
2017-01-22 05:07:34 -0500

    nope?

--- Assets/Code/Menu/MainMenuCanvasController.cs
+++ Assets/Code/Menu/MainMenuCanvasController.cs
@@ -138,10 +138,12 @@ namespace Assets.Code.Menu
         {
             Debug.Log(extendedInfo);
 
-            _network.StartClient();
+
             ClientScene.Ready(_network.client.connection);
             if (ClientScene.localPlayers.Count == 0)
                 ClientScene.AddPlayer(0);
+
+            _network.StartClient();
             _gameSession.StartSession(new GameSession
             {
                 OnExit = () =>
