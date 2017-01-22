751db19cd02639b0aa79c7f96bb38c007df72606
AsherLaakes <asherl@live.ca>
2017-01-22 05:28:14 -0500

    cursors should work properly now

--- Assets/Code/Player/Cursor.cs
+++ Assets/Code/Player/Cursor.cs
@@ -12,13 +12,6 @@ namespace Assets.Code.Player
 
         [AutoResolve] private GroundRaycaster _groundCast;
 
-        // lerpy lerps
-        private float _lastSynchronizationTime = 0f;
-        private float _syncDelay = 0f;
-        private float _syncTime = 0f;
-        private Vector3 _syncStartPosition = Vector3.zero;
-        private Vector3 _syncEndPosition = Vector3.zero;
-
         protected void Awake()
         {
             Resolver.AutoResolve(this);
@@ -31,40 +24,12 @@ namespace Assets.Code.Player
 
         protected void Update()
         {
-            if(_netId.localPlayerAuthority) HandleInput();
-            else HandleLerp();
+            if(isLocalPlayer) HandleInput();
         }
 
         private void HandleInput()
         {
             transform.position = _groundCast.GetMouseGroundPosition(UnityEngine.Input.mousePosition);
-        }
-
-        private void HandleLerp()
-        {
-            _syncTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(_syncStartPosition, _syncEndPosition, _syncTime / _syncDelay);
-        }
-
-        protected void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
-        {
-            Vector3 syncPosition = Vector3.zero;
-            if (stream.isWriting)
-            {
-                syncPosition = transform.position;
-                stream.Serialize(ref syncPosition);
-            }
-            else
-            {
-                stream.Serialize(ref syncPosition);
-
-                _syncTime = 0f;
-                _syncDelay = Time.time - _lastSynchronizationTime;
-                _lastSynchronizationTime = Time.time;
-
-                _syncStartPosition = transform.position;
-                _syncEndPosition = syncPosition;
-            }
         }
     }
 }
